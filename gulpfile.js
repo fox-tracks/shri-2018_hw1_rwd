2d69e61deea912da013d93dc179f6ec3e14fe463
Andryanova <andryanova@telemix.org.ru>
2018-10-02 15:30:50 +0300

    init commit, main project structure

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const htmlmin = require('gulp-htmlmin');
+const autoprefixer = require('autoprefixer');
+const minify = require('gulp-csso');
+const imagemin = require('gulp-imagemin');
+// const webp = require('gulp-webp');
+const rename = require('gulp-rename');
+const server = require('browser-sync').create();
+const run = require('run-sequence');
+const del = require('del');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+
+gulp.task('style', function () {
+  gulp.src('src/sass/styles.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer(
+        {
+          grid: true
+        }
+      )
+    ]))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(server.stream());
+});
+
+gulp.task('html', function () {
+  return gulp.src('*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', function () {
+  return gulp.src('src/js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('src/img/**/*.{png,jpg,svg}')
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('serve', function () {
+  server.init({
+    server: 'dist/'
+  });
+  gulp.watch('src/sass/**/*.scss', ['style']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('*.html', ['html']);
+});
+
+
+gulp.task('clean', function () {
+  return del('dist');
+});
+
+gulp.task('build', function (done) {
+  run(
+    'clean',
+    'style',
+    'html',
+    'scripts',
+    done);
+});
+
+gulp.task('build-full', function (done) {
+  run(
+    'build',
+    'images',
+    done);
+});
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// скрипты
+
+(function() {
+  // открытие/закрытие главного меню
+  const hamburgerBtn = document.querySelector('.hamburger-btn');
+  const mainNav = document.querySelector('.header__nav');
+
+  hamburgerBtn.addEventListener('click', ()=>{
+    mainNav.classList.toggle('header__nav_state_shown');
+    hamburgerBtn.classList.toggle('hamburger-btn_action_close')
+  });
+})();
