03072b578e6b1d8f585b11da026eb779e9406433
fox-tracks <t.andriyanova@yahoo.com>
2018-10-06 00:51:04 +0300

    add checking - is touch screen - add styles for non-touch

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,23 @@
 // скрипты
 
-(function() {
+(function () {
+  // определение тач-устройства
+
+
+  const isTouchCapable = 'ontouchstart' in window ||  window.DocumentTouch && document instanceof window.DocumentTouch ||
+    navigator.maxTouchPoints > 0 ||  window.navigator.msMaxTouchPoints > 0;
+
+  const cardContainer = document.querySelector('.event-feed__wrapper');
+
+  if (!isTouchCapable) {
+    cardContainer.classList.add('event-feed-not-touchscreen');
+  }
+
   // открытие/закрытие главного меню
   const hamburgerBtn = document.querySelector('.hamburger-btn');
   const mainNav = document.querySelector('.header__nav');
 
-  hamburgerBtn.addEventListener('click', ()=>{
+  hamburgerBtn.addEventListener('click', () => {
     mainNav.classList.toggle('header__nav_state_shown');
     hamburgerBtn.classList.toggle('hamburger-btn_action_close')
   });
@@ -13,172 +25,171 @@
 
   // карточка события
   const events = [
-        {
-            "type": "info",
-            "title": "Еженедельный отчет по расходам ресурсов",
-            "source": "Сенсоры потребления",
-            "time": "19:00, Сегодня",
-            "description": "Так держать! За последнюю неделю вы потратили на 10% меньше ресурсов, чем неделей ранее.",
-            "icon": "stats",
-            "data": {
-                "type": "graph",
-                "values": [
-                    {
-                        "electricity": [
-                            ["1536883200", 115],
-                            ["1536969600", 117],
-                            ["1537056000", 117.2],
-                            ["1537142400", 118],
-                            ["1537228800", 120],
-                            ["1537315200", 123],
-                            ["1537401600", 129]
-                        ]
-                    },
-                    {
-                        "water": [
-                            ["1536883200", 40],
-                            ["1536969600", 40.2],
-                            ["1537056000", 40.5],
-                            ["1537142400", 41],
-                            ["1537228800", 41.4],
-                            ["1537315200", 41.9],
-                            ["1537401600", 42.6]
-                        ]
-                    },
-                    {
-                        "gas": [
-                            ["1536883200", 13],
-                            ["1536969600", 13.2],
-                            ["1537056000", 13.5],
-                            ["1537142400", 13.7],
-                            ["1537228800", 14],
-                            ["1537315200", 14.2],
-                            ["1537401600", 14.5]
-                        ]
-                    }
-                ]
-            },
-            "size": "l"
+    {
+      "type": "info",
+      "title": "Еженедельный отчет по расходам ресурсов",
+      "source": "Сенсоры потребления",
+      "time": "19:00, Сегодня",
+      "description": "Так держать! За последнюю неделю вы потратили на 10% меньше ресурсов, чем неделей ранее.",
+      "icon": "stats",
+      "data": {
+        "type": "graph",
+        "values": [
+          {
+            "electricity": [
+              ["1536883200", 115],
+              ["1536969600", 117],
+              ["1537056000", 117.2],
+              ["1537142400", 118],
+              ["1537228800", 120],
+              ["1537315200", 123],
+              ["1537401600", 129]
+            ]
+          },
+          {
+            "water": [
+              ["1536883200", 40],
+              ["1536969600", 40.2],
+              ["1537056000", 40.5],
+              ["1537142400", 41],
+              ["1537228800", 41.4],
+              ["1537315200", 41.9],
+              ["1537401600", 42.6]
+            ]
+          },
+          {
+            "gas": [
+              ["1536883200", 13],
+              ["1536969600", 13.2],
+              ["1537056000", 13.5],
+              ["1537142400", 13.7],
+              ["1537228800", 14],
+              ["1537315200", 14.2],
+              ["1537401600", 14.5]
+            ]
+          }
+        ]
+      },
+      "size": "l"
+    },
+    {
+      "type": "info",
+      "title": "Дверь открыта",
+      "source": "Сенсор входной двери",
+      "time": "18:50, Сегодня",
+      "description": null,
+      "icon": "key",
+      "size": "s"
+    },
+    {
+      "type": "info",
+      "title": "Уборка закончена",
+      "source": "Пылесос",
+      "time": "18:45, Сегодня",
+      "description": null,
+      "icon": "robot-cleaner",
+      "size": "s"
+    },
+    {
+      "type": "info",
+      "title": "Новый пользователь",
+      "source": "Роутер",
+      "time": "18:45, Сегодня",
+      "description": null,
+      "icon": "router",
+      "size": "s"
+    },
+    {
+      "type": "info",
+      "title": "Изменен климатический режим",
+      "source": "Сенсор микроклимата",
+      "time": "18:30, Сегодня",
+      "description": "Установлен климатический режим «Фиджи»",
+      "icon": "thermal",
+      "size": "m",
+      "data": {
+        "temperature": 24,
+        "humidity": 80
+      }
+    },
+    {
+      "type": "critical",
+      "title": "Невозможно включить кондиционер",
+      "source": "Кондиционер",
+      "time": "18:21, Сегодня",
+      "description": "В комнате открыто окно, закройте его и повторите попытку",
+      "icon": "ac",
+      "size": "m"
+    },
+    {
+      "type": "info",
+      "title": "Музыка включена",
+      "source": "Яндекс.Станция",
+      "time": "18:16, Сегодня",
+      "description": "Сейчас проигрывается:",
+      "icon": "music",
+      "size": "m",
+      "data": {
+        "albumcover": "https://avatars.yandex.net/get-music-content/193823/1820a43e.a.5517056-1/m1000x1000",
+        "artist": "Florence & The Machine",
+        "track": {
+          "name": "Big God",
+          "length": "4:31"
         },
-        {
-            "type": "info",
-            "title": "Дверь открыта",
-            "source": "Сенсор входной двери",
-            "time": "18:50, Сегодня",
-            "description": null,
-            "icon": "key",
-            "size": "s"
-        },
-        {
-            "type": "info",
-            "title": "Уборка закончена",
-            "source": "Пылесос",
-            "time": "18:45, Сегодня",
-            "description": null,
-            "icon": "robot-cleaner",
-            "size": "s"
-        },
-        {
-            "type": "info",
-            "title": "Новый пользователь",
-            "source": "Роутер",
-            "time": "18:45, Сегодня",
-            "description": null,
-            "icon": "router",
-            "size": "s"
-        },
-        {
-            "type": "info",
-            "title": "Изменен климатический режим",
-            "source": "Сенсор микроклимата",
-            "time": "18:30, Сегодня",
-            "description": "Установлен климатический режим «Фиджи»",
-            "icon": "thermal",
-            "size": "m",
-            "data": {
-                "temperature": 24,
-                "humidity": 80
-            }
-        },
-        {
-            "type": "critical",
-            "title": "Невозможно включить кондиционер",
-            "source": "Кондиционер",
-            "time": "18:21, Сегодня",
-            "description": "В комнате открыто окно, закройте его и повторите попытку",
-            "icon": "ac",
-            "size": "m"
-        },
-        {
-            "type": "info",
-            "title": "Музыка включена",
-            "source": "Яндекс.Станция",
-            "time": "18:16, Сегодня",
-            "description": "Сейчас проигрывается:",
-            "icon": "music",
-            "size": "m",
-            "data": {
-                "albumcover": "https://avatars.yandex.net/get-music-content/193823/1820a43e.a.5517056-1/m1000x1000",
-                "artist": "Florence & The Machine",
-                "track": {
-                    "name": "Big God",
-                    "length": "4:31"
-                },
-                "volume": 80
-            }
-        },
-        {
-            "type": "info",
-            "title": "Заканчивается молоко",
-            "source": "Холодильник",
-            "time": "17:23, Сегодня",
-            "description": "Кажется, в холодильнике заканчивается молоко. Вы хотите добавить его в список покупок?",
-            "icon": "fridge",
-            "size": "m",
-            "data": {
-                "buttons": ["Да", "Нет"]
-            }
-        },
-        {
-            "type": "info",
-            "title": "Зарядка завершена",
-            "source": "Оконный сенсор",
-            "time": "16:22, Сегодня",
-            "description": "Ура! Устройство «Оконный сенсор» снова в строю!",
-            "icon": "battery",
-            "size": "s"
-        },
-        {
-            "type": "critical",
-            "title": "Пылесос застрял",
-            "source": "Сенсор движения",
-            "time": "16:17, Сегодня",
-            "description": "Робопылесос не смог сменить свое местоположение в течение последних 3 минут. Похоже, ему нужна помощь.",
-            "icon": "cam",
-            "data": {
-                "image": "get_it_from_mocks_:3.jpg"
-            },
-            "size": "l"
-        },
-        {
-            "type": "info",
-            "title": "Вода вскипела",
-            "source": "Чайник",
-            "time": "16:20, Сегодня",
-            "description": null,
-            "icon": "kettle",
-            "size": "s"
-        }
-    ];
+        "volume": 80
+      }
+    },
+    {
+      "type": "info",
+      "title": "Заканчивается молоко",
+      "source": "Холодильник",
+      "time": "17:23, Сегодня",
+      "description": "Кажется, в холодильнике заканчивается молоко. Вы хотите добавить его в список покупок?",
+      "icon": "fridge",
+      "size": "m",
+      "data": {
+        "buttons": ["Да", "Нет"]
+      }
+    },
+    {
+      "type": "info",
+      "title": "Зарядка завершена",
+      "source": "Оконный сенсор",
+      "time": "16:22, Сегодня",
+      "description": "Ура! Устройство «Оконный сенсор» снова в строю!",
+      "icon": "battery",
+      "size": "s"
+    },
+    {
+      "type": "critical",
+      "title": "Пылесос застрял",
+      "source": "Сенсор движения",
+      "time": "16:17, Сегодня",
+      "description": "Робопылесос не смог сменить свое местоположение в течение последних 3 минут. Похоже, ему нужна помощь.",
+      "icon": "cam",
+      "data": {
+        "image": "get_it_from_mocks_:3.jpg"
+      },
+      "size": "l"
+    },
+    {
+      "type": "info",
+      "title": "Вода вскипела",
+      "source": "Чайник",
+      "time": "16:20, Сегодня",
+      "description": null,
+      "icon": "kettle",
+      "size": "s"
+    }
+  ];
   const template = document.querySelector('template').content.querySelector('article.event-feed__card');
-  const cardContainer = document.querySelector('.event-feed__wrapper');
 
   // генерация виджетов
 
   // график
   // todo chart.js(canvas, new chart, data, options)
 
-  function createGraph () {
+  function createGraph() {
     const IMG_PATH = 'img/'; // относительный путь до картинок
     const IMG_NAMES = [
       'Richdata@1x.png',
@@ -190,7 +201,7 @@
       const srcs = []; // переименовать
       srcs.push(IMG_PATH + img);
 
-      return srcs ;
+      return srcs;
     }); // массив src
 
     console.log(IMG_SRCS);
@@ -202,19 +213,18 @@
 
   // климатические характеристики
   function getClimateParams(data) {
-    const { temperature, humidity } = data;
+    const {temperature, humidity} = data;
 
-    return (`<div class="widget__wrap climat">
-        <div class="climat__temperature">Температура: <span class="climat__temperature-value">${temperature} С</span></div>
-        <div class="climat__humidity">Влажность : <span class="climat__humidity-value">${humidity}%</span></div>
+    return (`<div class="widget__wrap climate">
+        <div class="climate__temperature">Температура: <span class="climate__temperature-value">${temperature} С</span></div>
+        <div class="climate__humidity">Влажность : <span class="climate__humidity-value">${humidity}%</span></div>
       </div>`);
   }
 
   // музыкальный плейер
   function createPlayer(data) {
-    const { albumcover, artist, volume } = data;
-
-    const { name, length } = data.track;
+    const {albumcover, artist, volume} = data;
+    const {name, length} = data.track;
 
     return (`<div class="widget__wrap player">
         <img class="player__album-cover" src="${albumcover}" width="52px" height="53px" alt="Florence & The Machine">
@@ -231,12 +241,12 @@
   }
 
   // диалоговая панель
-  function createDialog(data) {
-    const { buttons } = data;
+  function createDialogue(data) {
+    const {buttons} = data;
 
-    return (`<div class="widget__wrap dialog">
-        <button class="dialog__btn dialog__btn_agree">${buttons[0]}</button>
-        <button class="dialog__btn dialog__btn_cancel">${buttons[1]}</button>
+    return (`<div class="widget__wrap dialogue">
+        <button class="dialogue__btn dialogue__btn_agree">${buttons[0]}</button>
+        <button class="dialogue__btn dialogue__btn_cancel">${buttons[1]}</button>
       </div>`);
   }
 
@@ -247,9 +257,9 @@
     return '<div class="widget__wrap widget__wrap_cam"></div>';
   }
 
-  function createCardByData (template, dataItem) {
+  function createCardByData(template, dataItem) {
     const card = template.cloneNode(true);
-    const { icon } = dataItem;
+    const {icon} = dataItem;
     const extraContent = card.querySelector('div.card__extra-content');
 
     card.classList.add('card_icon_' + icon);
@@ -261,7 +271,7 @@
     card.querySelector('p.card__source').textContent = dataItem.source;
     card.querySelector('p.card__time').textContent = dataItem.time;
 
-    if(dataItem.description !== null) {
+    if (dataItem.description !== null) {
       let description = document.createElement('div');
       description.classList.add('card__description');
       description.textContent = dataItem.description;
@@ -272,7 +282,7 @@
     }
 
 
-    if(dataItem.hasOwnProperty('data')) {
+    if (dataItem.hasOwnProperty('data')) {
       let widget = document.createElement('div');
       widget.classList.add('card__widget');
       widget.classList.add('widget');
@@ -280,24 +290,24 @@
       const {data} = dataItem;
 
       switch (icon) {
-      case 'stats':
-        widget.innerHTML = createGraph(data);
-        console.log(widget);
-        break;
-      case 'thermal':
-        widget.innerHTML = getClimateParams(data);
-        break;
-      case 'music':
-        widget.innerHTML = createPlayer(data);
-        break;
-      case 'fridge':
-        widget.innerHTML = createDialog(data);
-        break;
-      case 'cam':
-        widget.innerHTML = createVideo();
-        break;
-      default:
-        break;
+        case 'stats':
+          widget.innerHTML = createGraph(data);
+          console.log(widget);
+          break;
+        case 'thermal':
+          widget.innerHTML = getClimateParams(data);
+          break;
+        case 'music':
+          widget.innerHTML = createPlayer(data);
+          break;
+        case 'fridge':
+          widget.innerHTML = createDialogue(data);
+          break;
+        case 'cam':
+          widget.innerHTML = createVideo();
+          break;
+        default:
+          break;
       }
       extraContent.appendChild(widget);
     }
@@ -316,7 +326,7 @@
     return fragment;
   }
 
-  function renderCards (data) {
+  function renderCards(data) {
     const fragment = createFragmentWithCards(data);
     cardContainer.appendChild(fragment);
 
