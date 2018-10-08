6908de50c12db6b5385ea8fd99ab128851272374
Andryanova <andryanova@telemix.org.ru>
2018-10-08 13:37:04 +0300

    add camera zoom gesture

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,115 +2,98 @@
 
 function initCameraGesture(){
 
-  // поворот камеры:
   const camera = document.querySelector('.camera');
-  const scroll = document.querySelector('.camera__scroll');
-  const cameraWidth = camera.getBoundingClientRect().width;
-  const cameraBgSize = getComputedStyle(camera).getPropertyValue('background-size');
+  let cameraWidth = camera.getBoundingClientRect().width;
+  let prevBgSize;
   let gesture = null;
-  const camState = {
-    startPositionX: 0,
-    startZoom: + (cameraBgSize.slice(0, -1))
-  };
+  let evCache = new Array();
+  let prevDiff = -1;
 
-  let evtStorage = new Array();
-  let prevDif = -1;
+    camera.onpointerdown = pointerdown_handler;
+    camera.onpointermove = pointermove_handler;
+    camera.onpointerup = pointerup_handler;
+    camera.onpointercancel = pointerup_handler;
+    camera.onpointerout = pointerup_handler;
+    camera.onpointerleave = pointerup_handler;
 
-  camera.addEventListener('pointerdown', evt => {
-    camera.style.transition = 'none';
-    camera.setPointerCapture(evt.pointerId);
+  function pointerdown_handler(ev) {
+    camera.setPointerCapture(ev.pointerId);
 
-    // формируем событие
+    // формируем жест
     gesture = {
-      id: evt.pointerId,
-      startX: evt.x,
-      startPositionX: camState.startPositionX
+      id: ev.pointerId,
+      startX: ev.x,
     };
 
     // записываем в массив
-    evtStorage.push(gesture);
+    evCache.push(gesture);
 
-  });
+    if (evCache.length === 2) {
+      prevDiff = (Math.abs(evCache[0].startX - evCache[1].startX)) * 100 / cameraWidth;
+      prevBgSize = + ((getComputedStyle(camera).getPropertyValue('background-size')).slice(0, -1));
+    }
+  }
 
-  camera.addEventListener('pointermove', evt => {
-    // если в массиве событий 2 реализуем zoom
-    if(evtStorage.length === 2) {
-
-      evtStorage.forEach(e => {
-        if(!gesture) {
-          return
-        }
-
-        // если такое событие уже есть в массиве переприсваиваем координаты
-        if (evt.pointerId === e.id) {
-          e.startX = evt.x;
-        }
-      });
-      const currentDif = Math.abs(evtStorage[0].startX - evtStorage[1].startX);
-
-      if (prevDif > 0) {
-        const zoom = (currentDif / cameraWidth) * 100;
-
-        if(currentDif > prevDif) {
-          camera.style.backgroundSize = camState.startZoom + zoom + '%';
-          camState.startZoom = camState.startZoom + zoom;
-        }
-
-        if(currentDif < prevDif) {
-          camera.style.backgroundSize = camState.startZoom - zoom + '%';
-          camState.startZoom = camState.startZoom - zoom;
-        }
-
-        prevDif = currentDif;
+  function pointermove_handler(ev) {
+    for (let i = 0; i < evCache.length; i++) {
+      if (ev.pointerId === evCache[i].id) {
+        evCache[i].startX = ev.x;
+        break;
       }
     }
-    // если событий < 2
-    if(evtStorage.length < 2) {
-      // проверем есть ли оно
-      if(!gesture) {
-        return
-      }
-      // если есть реализуем поворот
-      camera.classList.add('camera_moved');
-      const cameraMoved = document.querySelector('.camera_moved');
-      const { startX, startPositionX } = gesture;
-      const { x } = evt;
-      let difX = x - startX;
 
-      const cameraBgSizeValue = camState.startZoom;
-      const cameraBgSizePx = (cameraWidth * cameraBgSizeValue) / 100;
-      const rate = cameraWidth / (cameraBgSizePx - cameraWidth);
+    if (evCache.length === 2) {
+      const curDiff = (Math.abs(evCache[0].startX - evCache[1].startX)) * 100 / cameraWidth;
+      let increase;
+      let currentBgSize;
+      if (prevDiff > 0) {
+        increase = curDiff - prevDiff;
+        currentBgSize = prevBgSize + increase;
+        camera.style.backgroundSize = currentBgSize + '%';
 
-      if((startPositionX + difX) >= 0) {
-        cameraMoved.style.backgroundPositionX = '0px';
-        camState.startPositionX = 0;
-        scroll.style.left = 0;
+        if (curDiff > prevDiff) {
+          ev.target.style.border = "5px dashed yellow";
+        }
+        if (curDiff < prevDiff) {
+          ev.target.style.border = "5px dashed black";
+        }
       }
-      else if ((startPositionX + difX) < 0 && Math.abs(startPositionX + difX) >= (cameraBgSizePx - cameraWidth)) {
-        cameraMoved.style.backgroundPositionX = '-' + (cameraBgSizePx - cameraWidth).toString() + 'px';
-        camState.startPositionX = - (cameraBgSizePx - cameraWidth);
-        scroll.style.left = (cameraWidth - scroll.getBoundingClientRect().width) + 'px';
-      }
-      else {
-        cameraMoved.style.backgroundPositionX = startPositionX + difX + 'px';
-        camState.startPositionX = startPositionX + difX;
-        scroll.style.left = Math.abs(startPositionX + difX) * rate + 'px';
+      prevDiff = curDiff;
+      prevBgSize = currentBgSize;
+    }
+
+    if(evCache.length < 2) {
+      ev.target.style.border = "5px dashed green";
+
+    }
+  }
+
+  function remove_event(ev) {
+    for (let i = 0; i < evCache.length; i++) {
+      if (evCache[i].id === ev.pointerId) {
+        evCache.splice(i, 1);
+        break;
       }
     }
-  });
+  }
 
-  camera.addEventListener('pointerup', () => {
-    gesture = null;
-  });
-  camera.addEventListener('pointercancel', () => {
-    gesture = null;
-  });
+  function pointerup_handler(ev) {
+    remove_event(ev);
+    ev.target.style.border = "2px solid blue";
 
-
-
+    if (evCache.length === 2) {
+      prevDiff = -1;
+      prevBgSize = + ((getComputedStyle(camera).getPropertyValue('background-size')).slice(0, -1));
+    }
+  }
 }
 
 setTimeout(initCameraGesture, 0);
+
+
+
+
+
 // скрипты
 'use strict';
 
--- src/js/camera-controls.js
+++ src/js/camera-controls.js
@@ -3,44 +3,63 @@
 function initCameraGesture(){
 
   const camera = document.querySelector('.camera');
+  let cameraWidth = camera.getBoundingClientRect().width;
+  let prevBgSize;
+  let gesture = null;
   let evCache = new Array();
   let prevDiff = -1;
 
-     camera.onpointerdown = pointerdown_handler;
+    camera.onpointerdown = pointerdown_handler;
     camera.onpointermove = pointermove_handler;
-
-
     camera.onpointerup = pointerup_handler;
     camera.onpointercancel = pointerup_handler;
     camera.onpointerout = pointerup_handler;
     camera.onpointerleave = pointerup_handler;
 
   function pointerdown_handler(ev) {
-    evCache.push(ev);
+    camera.setPointerCapture(ev.pointerId);
+
+    // формируем жест
+    gesture = {
+      id: ev.pointerId,
+      startX: ev.x,
+    };
+
+    // записываем в массив
+    evCache.push(gesture);
+
+    if (evCache.length === 2) {
+      prevDiff = (Math.abs(evCache[0].startX - evCache[1].startX)) * 100 / cameraWidth;
+      prevBgSize = + ((getComputedStyle(camera).getPropertyValue('background-size')).slice(0, -1));
+    }
   }
 
   function pointermove_handler(ev) {
-    ev.target.style.border = 'green';
-
     for (let i = 0; i < evCache.length; i++) {
-      if (ev.pointerId === evCache[i].pointerId) {
-        evCache[i] = ev;
+      if (ev.pointerId === evCache[i].id) {
+        evCache[i].startX = ev.x;
         break;
       }
     }
 
-     if (evCache.length === 2) {
-      const curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
-
+    if (evCache.length === 2) {
+      const curDiff = (Math.abs(evCache[0].startX - evCache[1].startX)) * 100 / cameraWidth;
+      let increase;
+      let currentBgSize;
       if (prevDiff > 0) {
+        increase = curDiff - prevDiff;
+        currentBgSize = prevBgSize + increase;
+        camera.style.backgroundSize = currentBgSize + '%';
+
         if (curDiff > prevDiff) {
           ev.target.style.border = "5px dashed yellow";
         }
         if (curDiff < prevDiff) {
-          ev.target.style.border = "5px dashed red";
+          ev.target.style.border = "5px dashed black";
         }
       }
       prevDiff = curDiff;
+      prevBgSize = currentBgSize;
     }
 
     if(evCache.length < 2) {
@@ -50,9 +69,8 @@ function initCameraGesture(){
   }
 
   function remove_event(ev) {
-    // Remove this event from the target's cache
     for (let i = 0; i < evCache.length; i++) {
-      if (evCache[i].pointerId === ev.pointerId) {
+      if (evCache[i].id === ev.pointerId) {
         evCache.splice(i, 1);
         break;
       }
@@ -63,131 +81,15 @@ function initCameraGesture(){
     remove_event(ev);
     ev.target.style.border = "2px solid blue";
 
-    if (evCache.length < 2) prevDiff = -1;
+    if (evCache.length === 2) {
+      prevDiff = -1;
+      prevBgSize = + ((getComputedStyle(camera).getPropertyValue('background-size')).slice(0, -1));
+    }
   }
 }
 
 setTimeout(initCameraGesture, 0);
 
 
-//
-// // проверем есть ли оно
-// if(!gesture) {
-//   return
-// }
-// // если есть реализуем поворот
-// camera.classList.add('camera_moved');
-// const cameraMoved = document.querySelector('.camera_moved');
-// const { startX, startPositionX } = gesture;
-// const { x } = evt;
-// let difX = x - startX;
-//
-// const cameraBgSizeValue = camState.startZoom;
-// const cameraBgSizePx = (cameraWidth * cameraBgSizeValue) / 100;
-// const rate = cameraWidth / (cameraBgSizePx - cameraWidth);
-//
-// if((startPositionX + difX) >= 0) {
-//   cameraMoved.style.backgroundPositionX = '0px';
-//   camState.startPositionX = 0;
-//   scroll.style.left = 0;
-// }
-// else if ((startPositionX + difX) < 0 && Math.abs(startPositionX + difX) >= (cameraBgSizePx - cameraWidth)) {
-//   cameraMoved.style.backgroundPositionX = '-' + (cameraBgSizePx - cameraWidth).toString() + 'px';
-//   camState.startPositionX = - (cameraBgSizePx - cameraWidth);
-//   scroll.style.left = (cameraWidth - scroll.getBoundingClientRect().width) + 'px';
-// }
-// else {
-//   cameraMoved.style.backgroundPositionX = startPositionX + difX + 'px';
-//   camState.startPositionX = startPositionX + difX;
-//   scroll.style.left = Math.abs(startPositionX + difX) * rate + 'px';
-// }
-// }
-//
-//
-//
-// function initCameraGesture(){
-//
-//   // поворот камеры:
-//   const camera = document.querySelector('.camera');
-//   // const scroll = document.querySelector('.camera__scroll');
-//   // const cameraWidth = camera.getBoundingClientRect().width;
-//   // const cameraBgSize = getComputedStyle(camera).getPropertyValue('background-size');
-//   let gesture = null;
-//   // const camState = {
-//   //   startPositionX: 0,
-//   //   startZoom: + (cameraBgSize.slice(0, -1))
-//   // };
-//
-//   let evtStorage = {};
-//   // let prevDif = -1;
-//
-//   camera.addEventListener('pointerdown', evt => {
-//     camera.style.transition = 'none';
-//     camera.setPointerCapture(evt.pointerId);
-//
-//     // формируем событие
-//     gesture = {
-//       id: evt.pointerId,
-//       startX: evt.x,
-//       startPositionX: camState.startPositionX
-//     };
-//
-//     // записываем в объект
-//     evtStorage[gesture.id] = gesture;
-//   });
-//
-//   camera.addEventListener('pointermove', evt => {
-//
-//     const keys = Object.keys(evtStorage);
-//
-//
-//     // если в массиве событий 2 реализуем zoom
-//     if (keys.length === 2) {
-//
-//
-//       alert(keys);
-      // evtStorage.forEach(e => {
-      //   if(!gesture) {
-      //     return
-      //   }
-      //
-      //   // если такое событие уже есть в массиве переприсваиваем координаты
-      //   if (evt.pointerId === e.id) {
-      //     e.startX = evt.x;
-      //   }
-      // });
-      // const currentDif = Math.abs(evtStorage[0].startX - evtStorage[1].startX);
-      //
-      // if (prevDif > 0) {
-      //   const zoom = (currentDif / cameraWidth) * 100;
-      //
-      //   if(currentDif > prevDif) {
-      //     camera.style.backgroundSize = camState.startZoom + zoom + '%';
-      //     camState.startZoom = camState.startZoom + zoom;
-      //   }
-      //
-      //   if(currentDif < prevDif) {
-      //     camera.style.backgroundSize = camState.startZoom - zoom + '%';
-      //     camState.startZoom = camState.startZoom - zoom;
-      //   }
-      //
-      //   prevDif = currentDif;
-      // }
-
-  //   }
-  // });
-  //
-  // function deleteGesture (evt) {
-  //   alert('!' + JSON.stringify(evt.pointerId) + JSON.stringify(evtStorage));
-  //   delete evtStorage[evt.pointerId];
-  //   alert('2' + JSON.stringify(evt.pointerId) + JSON.stringify(evtStorage));
-  // }
-  // camera.addEventListener('pointerup', deleteGesture);
-  // camera.addEventListener('pointercancel', deleteGesture);
-  // camera.addEventListener('pointerout', deleteGesture);
-  // camera.addEventListener('pointerleave', deleteGesture);
 
 
-
-
-}
